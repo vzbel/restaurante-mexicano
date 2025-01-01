99e066f0c280ae0038824a639e0f4b624a1f44ac
vzbel <142253332+vzbel@users.noreply.github.com>
2024-12-31 19:05:48 -0700

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Nav from './Nav'
+
+function App() {
+  return (
+    <>
+      <Nav />
+    </>
+  );
+}
+
+export default App
---
+++ src/Nav.jsx
@@ -0,0 +1,17 @@
+function Nav(){
+    return (
+        <nav>
+            {/* Nav Links UL*/}
+            <ul>
+                <li><a href="">ABOUT</a></li>
+                <li><a href="">MENU</a></li>
+                <li><a href="">LOCATIONS</a></li>
+            </ul>
+
+            {/* CTA Button*/}
+            <button>CALL NOW</button>
+        </nav>
+    );
+}
+
+export default Nav
