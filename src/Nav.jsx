7890eaecb086846256ee97f078afd5b1e47fa1a9
vzbel <142253332+vzbel@users.noreply.github.com>
2024-12-31 20:44:40 -0700

    Add navigation bar

--- src/Nav.jsx
+++ src/Nav.jsx
@@ -1,15 +1,15 @@
 function Nav(){
     return (
-        <nav>
+        <nav className="flex flex-col md:flex-row gap-4 items-center flex-wrap justify-between py-10 px-20 fixed top-0 w-screen font-mono font-semibold text-xl">
             {/* Nav Links UL*/}
-            <ul>
+            <ul className="flex gap-5">
                 <li><a href="">ABOUT</a></li>
                 <li><a href="">MENU</a></li>
                 <li><a href="">LOCATIONS</a></li>
             </ul>
 
             {/* CTA Button*/}
-            <button>CALL NOW</button>
+            <button className="whitespace-nowrap outline outline-lime-700 py-1 px-8 rounded-md ">ORDER NOW</button>
         </nav>
     );
 }
