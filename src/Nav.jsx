73449dbf819ff2245f19ec70f83c171dae550807
vzbel <142253332+vzbel@users.noreply.github.com>
2025-01-02 18:17:02 -0700

    Change navbar to sticky position

--- src/Nav.jsx
+++ src/Nav.jsx
@@ -1,15 +1,15 @@
 function Nav(){
     return (
-        <nav className="flex flex-col md:flex-row gap-4 items-center flex-wrap justify-between py-10 px-20 fixed top-0 w-screen font-mono font-semibold text-xl">
+        <nav className="flex flex-col md:flex-row gap-4 items-center flex-wrap justify-center md:justify-between px-24 sticky top-0 w-screen font-mono font-semibold text-xl min-h-32">
             {/* Nav Links UL*/}
-            <ul className="flex gap-5">
+            <ul className="flex gap-7">
                 <li><a href="">ABOUT</a></li>
                 <li><a href="">MENU</a></li>
                 <li><a href="">LOCATIONS</a></li>
             </ul>
 
             {/* CTA Button*/}
-            <button className="whitespace-nowrap outline outline-lime-700 py-1 px-8 rounded-md ">ORDER NOW</button>
+            <button className="whitespace-nowrap outline outline-lime-800 py-1 px-8 rounded-md ">ORDER NOW</button>
         </nav>
     );
 }
